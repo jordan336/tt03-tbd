b35a120ef1af229ed38915ba7f6330df7c710035
Jordan Ebel <jordan336900@gmail.com>
2023-03-31 21:44:49 -0700

    Add tests for each opcode

--- src/test.py
+++ src/test.py
@@ -4,13 +4,49 @@ import random
 
 @cocotb.test()
 async def test_nop(dut):
-    dut._log.info("Test NOP")
-
-    dut.io_in.value = 0
+    dut.io_in.value = 0x0
     await Timer(1, units="ns")
-
-    dut._log.info("io_in: %s", dut.io_in.value)
-    dut._log.info("io_out: %s", dut.io_out.value)
-
     assert int(dut.io_out.value) == 1
+
+@cocotb.test()
+async def test_add(dut):
+    dut.io_in.value = 0x20
+    await Timer(1, units="ns")
+    assert int(dut.io_out.value) == 2
+
+@cocotb.test()
+async def test_addi(dut):
+    dut.io_in.value = 0x40
+    await Timer(1, units="ns")
+    assert int(dut.io_out.value) == 4
+
+@cocotb.test()
+async def test_and(dut):
+    dut.io_in.value = 0x60
+    await Timer(1, units="ns")
+    assert int(dut.io_out.value) == 8
+
+@cocotb.test()
+async def test_andi(dut):
+    dut.io_in.value = 0x80
+    await Timer(1, units="ns")
+    assert int(dut.io_out.value) == 16
+
+@cocotb.test()
+async def test_or(dut):
+    dut.io_in.value = 0xa0
+    await Timer(1, units="ns")
+    assert int(dut.io_out.value) == 32
+
+@cocotb.test()
+async def test_ori(dut):
+    dut.io_in.value = 0xc0
+    await Timer(1, units="ns")
+    assert int(dut.io_out.value) == 64
+
+@cocotb.test()
+async def test_store(dut):
+    dut.io_in.value = 0xe0
+    await Timer(1, units="ns")
+    assert int(dut.io_out.value) == 128
 
