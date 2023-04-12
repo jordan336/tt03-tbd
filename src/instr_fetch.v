90b64e99ad95bf2c1cd8512aa42dd6d0d0aada3a
Jordan Ebel <jordan336900@gmail.com>
2023-04-11 22:47:33 -0700

    Add fetch state machine. Add fetch tests.

--- src/instr_fetch.v
+++ src/instr_fetch.v
@@ -7,9 +7,111 @@ module instr_fetch (input            clk,
                     output reg [2:0] src_b,
                     output reg [2:0] dest,
                     output reg [7:0] imm);
-  assign src_a = in[2:0];
+
+  localparam STATE_START = 2'b00;
+  localparam STATE_ONE   = 2'b01;
+  localparam STATE_TWO   = 2'b10;
+  localparam STATE_THREE = 2'b11;
+
+  reg [1:0] state, new_state;
+  reg load_opcode;
+  reg load_src_a;
+  reg load_src_b;
+  reg load_dest;
+  reg load_imm_upper;
+  reg load_imm_lower;
+  reg op_valid_next;
+
+  always @(*) begin
+    new_state = state;
+    load_opcode = 0;
+    load_src_a = 0;
+    load_src_b = 0;
+    load_dest = 0;
+    load_imm_upper = 0;
+    load_imm_lower = 0;
+    op_valid_next = 0;
+
+    case (state)
+      STATE_START: begin
+        if (in === '1) begin
+          new_state = STATE_ONE;
+        end
+      end
+      STATE_ONE: begin
+        load_opcode = 1;
+        load_src_a = 1;
+        if (in[5:3] === 3'b000) begin
+          new_state = STATE_ONE;
+          op_valid_next = 1;
+        end else begin
+          new_state = STATE_TWO;
+        end
+      end
+      STATE_TWO: begin
+        load_dest = 1;
+        if ((opcode === 3'b010) ||
+            (opcode === 3'b100) ||
+            (opcode === 3'b110) ||
+            (opcode === 3'b111)) begin
+          load_imm_upper = 1;
+          new_state = STATE_THREE;
+        end else begin
+          load_src_b = 1;
+          op_valid_next = 1;
+          new_state = STATE_ONE;
+        end
+      end
+      STATE_THREE: begin
+        load_imm_lower = 1;
+        op_valid_next = 1;
+        new_state = STATE_ONE;
+      end
+    endcase
+  end
+
+  always @(posedge clk, posedge rst) begin
+    if (rst == 1'b1) begin
+      state <= STATE_START;
+    end else begin
+      state <= new_state;
+    end
+  end
 
   always @(posedge clk) begin
-    opcode <= in[5:3];
+    if (load_opcode) begin
+      opcode <= in[5:3];
+    end
+  end
+
+  always @(posedge clk) begin
+    if (load_src_a) begin
+      src_a <= in[2:0];
+    end
+  end
+
+  always @(posedge clk) begin
+    if (load_src_b) begin
+      src_b <= in[2:0];
+    end
+  end
+
+  always @(posedge clk) begin
+    if (load_dest) begin
+      dest <= in[5:3];
+    end
+  end
+
+  always @(posedge clk) begin
+    if (load_imm_lower) begin
+      imm[4:0] <= in[4:0];
+    end
+    if (load_imm_upper) begin
+      imm[7:5] <= in[2:0];
+    end
+  end
+
+  always @(posedge clk) begin
+    op_valid <= op_valid_next;
   end
 endmodule
--- src/test.py
+++ src/test.py
@@ -1,63 +1,217 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import FallingEdge, Timer
 from cocotb.clock import Clock
+from cocotb.binary import BinaryValue, BinaryRepresentation
 import random
 
 CLK_FREQ_HZ = 10000
 CLK_PERIOD_SEC = (1 / CLK_FREQ_HZ)
 CLK_PERIOD_US = CLK_PERIOD_SEC * 1000000
 
+def start_clk(dut):
+    cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
+
+def split_imm(imm):
+    imm_75 = (imm & 0xe0) >> 5
+    imm_43 = (imm & 0x18) >> 3
+    imm_20 = imm & 0x7
+    return imm_75, imm_43, imm_20
+
+async def exit_reset(dut):
+    dut.rst.value = 1;
+    await FallingEdge(dut.clk)
+    dut.rst.value = 0;
+    await FallingEdge(dut.clk)
+
+async def exit_start_state(dut):
+    dut.instr.value = 0x3f
+    await FallingEdge(dut.clk)
+
+async def do_preamble(dut):
+    start_clk(dut)
+    await exit_reset(dut)
+    await exit_start_state(dut)
+
+async def drive_instr(dut, bits35, bits02):
+    instr_val = BinaryValue(n_bits=6, bigEndian=False, binaryRepresentation=BinaryRepresentation.UNSIGNED)
+    instr_val.binstr = '{:03b}{:03b}'.format(bits35, bits02)
+    dut.instr.value = instr_val
+    await FallingEdge(dut.clk)
+
+async def execute_disp(dut, src):
+    await drive_instr(dut, 0, src)
+
+async def execute_add(dut, dest, src_a, src_b):
+    await drive_instr(dut, 1, src_a)
+    await drive_instr(dut, dest, src_b)
+
+async def execute_add_i(dut, dest, src, imm):
+    imm_75 = (imm & 0xe0) >> 5
+    imm_43 = (imm & 0x18) >> 3
+    imm_20 = imm & 0x7
+    await drive_instr(dut, 2, src)
+    await drive_instr(dut, dest, imm_75)
+    await drive_instr(dut, imm_43, imm_20)
+
+async def execute_and(dut, dest, src_a, src_b):
+    await drive_instr(dut, 3, src_a)
+    await drive_instr(dut, dest, src_b)
+
+async def execute_and_i(dut, dest, src, imm):
+    imm_75, imm_43, imm_20 = split_imm(imm)
+    await drive_instr(dut, 4, src)
+    await drive_instr(dut, dest, imm_75)
+    await drive_instr(dut, imm_43, imm_20)
+
+async def execute_or(dut, dest, src_a, src_b):
+    await drive_instr(dut, 5, src_a)
+    await drive_instr(dut, dest, src_b)
+
+async def execute_or_i(dut, dest, src, imm):
+    imm_75, imm_43, imm_20 = split_imm(imm)
+    await drive_instr(dut, 6, src)
+    await drive_instr(dut, dest, imm_75)
+    await drive_instr(dut, imm_43, imm_20)
+
+async def execute_store(dut, dest, imm):
+    imm_75, imm_43, imm_20 = split_imm(imm)
+    await drive_instr(dut, 7, 0)
+    await drive_instr(dut, dest, imm_75)
+    await drive_instr(dut, imm_43, imm_20)
+
+def check_fetch(dut, opcode, dest=None, src_a=None, src_b=None, imm=None):
+    assert int(dut.toy.fetch.op_valid) == 1
+    assert int(dut.toy.fetch.opcode) == opcode
+    if dest is not None:
+        assert int(dut.toy.fetch.dest) == dest
+    if src_a is not None:
+        assert int(dut.toy.fetch.src_a) == src_a
+    if src_b is not None:
+        assert int(dut.toy.fetch.src_b) == src_b
+    if imm is not None:
+        assert int(dut.toy.fetch.imm) == imm
+
 @cocotb.test()
-async def test_nop(dut):
+async def fetch_disp(dut):
+    src = random.randint(0, 7)
+    await do_preamble(dut)
+    await execute_disp(dut, src)
+    check_fetch(dut, opcode=0, src_a=src)
+
+@cocotb.test()
+async def fetch_add(dut):
+    dest = random.randint(0, 7)
+    src_a = random.randint(0, 7)
+    src_b = random.randint(0, 7)
+    await do_preamble(dut)
+    await execute_add(dut, dest, src_a, src_b)
+    check_fetch(dut, 1, dest=dest, src_a=src_a, src_b=src_b)
+
+@cocotb.test()
+async def fetch_add_i(dut):
+    dest = random.randint(0, 7)
+    src = random.randint(0, 7)
+    imm = random.randint(0, 255)
+    await do_preamble(dut)
+    await execute_add_i(dut, dest, src, imm)
+    check_fetch(dut, 2, dest=dest, src_a=src, imm=imm)
+
+@cocotb.test()
+async def fetch_and(dut):
+    dest = random.randint(0, 7)
+    src_a = random.randint(0, 7)
+    src_b = random.randint(0, 7)
+    await do_preamble(dut)
+    await execute_and(dut, dest, src_a, src_b)
+    check_fetch(dut, 3, dest=dest, src_a=src_a, src_b=src_b)
+
+@cocotb.test()
+async def fetch_and_i(dut):
+    dest = random.randint(0, 7)
+    src = random.randint(0, 7)
+    imm = random.randint(0, 255)
+    await do_preamble(dut)
+    await execute_and_i(dut, dest, src, imm)
+    check_fetch(dut, 4, dest=dest, src_a=src, imm=imm)
+
+@cocotb.test()
+async def fetch_or(dut):
+    dest = random.randint(0, 7)
+    src_a = random.randint(0, 7)
+    src_b = random.randint(0, 7)
+    await do_preamble(dut)
+    await execute_or(dut, dest, src_a, src_b)
+    check_fetch(dut, 5, dest=dest, src_a=src_a, src_b=src_b)
+
+@cocotb.test()
+async def fetch_or_i(dut):
+    dest = random.randint(0, 7)
+    src = random.randint(0, 7)
+    imm = random.randint(0, 255)
+    await do_preamble(dut)
+    await execute_or_i(dut, dest, src, imm)
+    check_fetch(dut, 6, dest=dest, src_a=src, imm=imm)
+
+@cocotb.test()
+async def fetch_store(dut):
+    dest = random.randint(0, 7)
+    imm = random.randint(0, 255)
+    await do_preamble(dut)
+    await execute_store(dut, dest, imm)
+    check_fetch(dut, 7, dest=dest, imm=imm)
+
+@cocotb.test(skip=True)
+async def disp(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0
     await Timer(CLK_PERIOD_US, units="us")
+    assert int(dut.toy.fetch.opcode) == 0
     assert int(dut.io_out.value) == 1
 
-@cocotb.test()
-async def test_add(dut):
+@cocotb.test(skip=True)
+async def add(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0x08
     await Timer(CLK_PERIOD_US, units="us")
     assert int(dut.io_out.value) == 2
 
-@cocotb.test()
-async def test_addi(dut):
+@cocotb.test(skip=True)
+async def add_i(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0x10
     await Timer(CLK_PERIOD_US, units="us")
     assert int(dut.io_out.value) == 4
 
-@cocotb.test()
-async def test_and(dut):
+@cocotb.test(skip=True)
+async def _and(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0x18
     await Timer(CLK_PERIOD_US, units="us")
     assert int(dut.io_out.value) == 8
 
-@cocotb.test()
-async def test_andi(dut):
+@cocotb.test(skip=True)
+async def and_i(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0x20
     await Timer(CLK_PERIOD_US, units="us")
     assert int(dut.io_out.value) == 16
 
-@cocotb.test()
-async def test_or(dut):
+@cocotb.test(skip=True)
+async def _or(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0x28
     await Timer(CLK_PERIOD_US, units="us")
     assert int(dut.io_out.value) == 32
 
-@cocotb.test()
-async def test_ori(dut):
+@cocotb.test(skip=True)
+async def or_i(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0x30
     await Timer(CLK_PERIOD_US, units="us")
     assert int(dut.io_out.value) == 64
 
-@cocotb.test()
-async def test_store(dut):
+@cocotb.test(skip=True)
+async def store(dut):
     cocotb.start_soon(Clock(dut.clk, CLK_PERIOD_US, "us").start())
     dut.instr.value = 0x38
     await Timer(CLK_PERIOD_US, units="us")
